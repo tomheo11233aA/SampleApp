e00761b048f422ad0b59ed93e0e5bfe53688a96b
faIIinlove <phucnamvan@gmail.com>
2024-03-07 12:13:40 +0700

    feat: :art: Config lại app.js với thêm image

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+// /**
+//  * @format
+//  */
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
 /**
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, Text } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import store, { persistor } from '@redux/store/store';
+import { Provider } from 'react-redux';
+import i18n from './src/language/i18n'
+import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
+        </Provider>
+    );
+};
+
+Text.defaultProps = Text.defaultProps || {}
+Text.defaultProps.allowFontScaling = false
+
+AppRegistry.registerComponent(appName, () => Root);
